1858d4b5288b91f671814191792927c73ac20646
jkbaker@us.ibm.com <jkbaker@us.ibm.com>
2018-11-20 22:50:51 -0700

    first commit

---
+++ GiveNTake/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveNTake.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello from MVC!");
+        }
+    }
+}
